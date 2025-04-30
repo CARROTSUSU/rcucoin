3df25f66c4af319f4051da77542b13cebfc89f11
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-05-01 05:05:47 +0800

    Update app.go

--- abci/app.go
+++ abci/app.go
@@ -7,44 +7,85 @@ import (
 	abcitypes "github.com/tendermint/tendermint/abci/types"
 )
 
-type RCUCoinApp struct {
+const MirocPerRCU = 1_000_000
+
+type RcuCoinApp struct {
 	abcitypes.BaseApplication
-	balances map[string]int64
+	balances map[string]int64 // disimpan dalam Miroc
 }
 
-func NewRCUCoinApp() *RCUCoinApp {
-	return &RCUCoinApp{
+func NewRcuCoinApp() *RcuCoinApp {
+	return &RcuCoinApp{
 		balances: map[string]int64{
-			"rcu1": 1000000,
-			"rcu2": 500000,
+			"genesis1": 100 * MirocPerRCU,
+			"genesis2": 50 * MirocPerRCU,
 		},
 	}
 }
 
-type Tx struct {
+// Struktur transaksi
+type TransferTx struct {
 	From   string `json:"from"`
 	To     string `json:"to"`
-	Amount int64  `json:"amount"` // dalam unit Miroc (1 RCU = 1,000,000 Miroc)
+	Amount int64  `json:"amount"` // dalam Miroc
 }
 
-func (app *RCUCoinApp) DeliverTx(req abcitypes.RequestDeliverTx) abcitypes.ResponseDeliverTx {
-	var tx Tx
-	if err := json.Unmarshal(req.Tx, &tx); err != nil {
-		return abcitypes.ResponseDeliverTx{Code: 1, Log: "Invalid TX format"}
-	}
-
-	if app.balances[tx.From] < tx.Amount {
-		return abcitypes.ResponseDeliverTx{Code: 1, Log: "Insufficient funds"}
-	}
-
-	app.balances[tx.From] -= tx.Amount
-	app.balances[tx.To] += tx.Amount
-
-	return abcitypes.ResponseDeliverTx{Code: 0, Log: "Transaction successful"}
-}
-
-func (app *RCUCoinApp) Info(req abcitypes.RequestInfo) abcitypes.ResponseInfo {
+func (app *RcuCoinApp) Info(req abcitypes.RequestInfo) abcitypes.ResponseInfo {
 	return abcitypes.ResponseInfo{
-		Data: "RCUCoin v1.0",
+		Data: fmt.Sprintf("RCUCoin ABCI - Powered by Miroc"),
 	}
+}
+
+func (app *RcuCoinApp) CheckTx(tx []byte) abcitypes.ResponseCheckTx {
+	return app.validateTx(tx)
+}
+
+func (app *RcuCoinApp) DeliverTx(tx []byte) abcitypes.ResponseDeliverTx {
+	return app.executeTx(tx)
+}
+
+func (app *RcuCoinApp) Query(req abcitypes.RequestQuery) abcitypes.ResponseQuery {
+	address := string(req.Data)
+	balance, ok := app.balances[address]
+	if !ok {
+		return abcitypes.ResponseQuery{Code: 1, Log: "Address not found"}
+	}
+
+	return abcitypes.ResponseQuery{
+		Code:  0,
+		Log:   "Balance retrieved",
+		Value: []byte(fmt.Sprintf("%d", balance)),
+	}
+}
+
+func (app *RcuCoinApp) validateTx(tx []byte) abcitypes.ResponseCheckTx {
+	var t TransferTx
+	if err := json.Unmarshal(tx, &t); err != nil {
+		return abcitypes.ResponseCheckTx{Code: 1, Log: fmt.Sprintf("Invalid JSON: %v", err)}
+	}
+
+	if t.Amount <= 0 {
+		return abcitypes.ResponseCheckTx{Code: 1, Log: "Amount must be positive"}
+	}
+
+	if app.balances[t.From] < t.Amount {
+		return abcitypes.ResponseCheckTx{Code: 1, Log: "Insufficient funds"}
+	}
+
+	return abcitypes.ResponseCheckTx{Code: 0}
+}
+
+func (app *RcuCoinApp) executeTx(tx []byte) abcitypes.ResponseDeliverTx {
+	var t TransferTx
+	if err := json.Unmarshal(tx, &t); err != nil {
+		return abcitypes.ResponseDeliverTx{Code: 1, Log: fmt.Sprintf("Invalid JSON: %v", err)}
+	}
+
+	app.balances[t.From] -= t.Amount
+	app.balances[t.To] += t.Amount
+
+	log := fmt.Sprintf("Transferred %d Miroc (%.6f RCU) from %s to %s",
+		t.Amount, float64(t.Amount)/float64(MirocPerRCU), t.From, t.To)
+
+	return abcitypes.ResponseDeliverTx{Code: 0, Log: log}
 }
