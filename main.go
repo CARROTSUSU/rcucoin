faa3d029e84b8dcd2fa6d40c5da5b4bf65f46bb9
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-05-01 06:03:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,122 +1,102 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "log"
-    "net"
 
-    "encoding/json"
     abci "github.com/tendermint/tendermint/abci/types"
     "github.com/tendermint/tendermint/abci/server"
-    "github.com/CARROTSUSU/rcucoin/abci" // Tukar ikut nama GitHub kau
 )
 
-// Struktur aplikasi RcpuCoin
 type RcpuCoinApp struct {
-	types.BaseApplication
-	balances map[string]int // Penyimpanan dalam ingatan untuk baki akaun
+    abci.BaseApplication
+    balances map[string]int
 }
 
-// Fungsi untuk mencipta aplikasi RcpuCoin baru
 func NewRcpuCoinApp() *RcpuCoinApp {
-	app := &RcpuCoinApp{
-		balances: make(map[string]int),
-	}
-
-	// Menambah beberapa akaun permulaan dengan baki
-	app.balances["address1"] = 1000
-	app.balances["address2"] = 500
-
-	return app
+    app := &RcpuCoinApp{
+        balances: make(map[string]int),
+    }
+    app.balances["address1"] = 1000
+    app.balances["address2"] = 500
+    return app
 }
 
-// Menerima dan memproses transaksi
-func (app *RcpuCoinApp) DeliverTx(tx types.RequestDeliverTx) types.ResponseDeliverTx {
-	var transfer struct {
-		From   string `json:"from"`
-		To     string `json:"to"`
-		Amount int    `json:"amount"`
-	}
+func (app *RcpuCoinApp) DeliverTx(tx abci.RequestDeliverTx) abci.ResponseDeliverTx {
+    var transfer struct {
+        From   string `json:"from"`
+        To     string `json:"to"`
+        Amount int    `json:"amount"`
+    }
 
-	// Mendekod transaksi
-	err := json.Unmarshal(tx.Tx, &transfer)
-	if err != nil {
-		return types.ResponseDeliverTx{
-			Code: 1,
-			Log:  fmt.Sprintf("Error decoding transaction: %v", err),
-		}
-	}
+    if err := json.Unmarshal(tx.Tx, &transfer); err != nil {
+        return abci.ResponseDeliverTx{
+            Code: 1,
+            Log:  fmt.Sprintf("Error decoding tx: %v", err),
+        }
+    }
 
-	// Validasi jika penghantar ada baki yang mencukupi
-	if app.balances[transfer.From] < transfer.Amount {
-		return types.ResponseDeliverTx{
-			Code: 1,
-			Log:  "Insufficient balance",
-		}
-	}
+    if app.balances[transfer.From] < transfer.Amount {
+        return abci.ResponseDeliverTx{
+            Code: 1,
+            Log:  "Insufficient balance",
+        }
+    }
 
-	// Mengemas kini baki akaun
-	app.balances[transfer.From] -= transfer.Amount
-	app.balances[transfer.To] += transfer.Amount
+    app.balances[transfer.From] -= transfer.Amount
+    app.balances[transfer.To] += transfer.Amount
 
-	// Log transaksi
-	fmt.Printf("Transfer %d RCPU Coin from %s to %s\n", transfer.Amount, transfer.From, transfer.To)
+    fmt.Printf("Transferred %d RCU from %s to %s\n", transfer.Amount, transfer.From, transfer.To)
 
-	// Menyediakan respons untuk transaksi yang berjaya
-	return types.ResponseDeliverTx{
-		Code: 0,
-		Log:  "Transfer successful",
-	}
+    return abci.ResponseDeliverTx{Code: 0, Log: "Transfer successful"}
 }
 
-// Fungsi untuk mendapatkan informasi aplikasi
-func (app *RcpuCoinApp) Info(req types.RequestInfo) types.ResponseInfo {
-	// Menyediakan maklumat aplikasi
-	return types.ResponseInfo{
-		Data:             "RCUCoin Blockchain",
-		Validators:       []types.Validator{},
-		LatestBlockHash:  []byte("latest-block-hash"),
-		LatestAppHash:    []byte("latest-app-hash"),
-	}
+func (app *RcpuCoinApp) CheckTx(tx abci.RequestCheckTx) abci.ResponseCheckTx {
+    var transfer struct {
+        From   string `json:"from"`
+        To     string `json:"to"`
+        Amount int    `json:"amount"`
+    }
+
+    if err := json.Unmarshal(tx.Tx, &transfer); err != nil {
+        return abci.ResponseCheckTx{
+            Code: 1,
+            Log:  fmt.Sprintf("Error decoding tx: %v", err),
+        }
+    }
+
+    if app.balances[transfer.From] < transfer.Amount {
+        return abci.ResponseCheckTx{
+            Code: 1,
+            Log:  "Insufficient balance",
+        }
+    }
+
+    return abci.ResponseCheckTx{Code: 0}
 }
 
-// Fungsi untuk memeriksa konsistensi dan keadaan blockchain
-func (app *RcpuCoinApp) CheckTx(tx types.RequestCheckTx) types.ResponseCheckTx {
-	var transfer struct {
-		From   string `json:"from"`
-		To     string `json:"to"`
-		Amount int    `json:"amount"`
-	}
-
-	// Mendekod transaksi
-	err := json.Unmarshal(tx.Tx, &transfer)
-	if err != nil {
-		return types.ResponseCheckTx{
-			Code: 1,
-			Log:  fmt.Sprintf("Error decoding transaction: %v", err),
-		}
-	}
-
-	// Validasi jika penghantar ada baki yang mencukupi
-	if app.balances[transfer.From] < transfer.Amount {
-		return types.ResponseCheckTx{
-			Code: 1,
-			Log:  "Insufficient balance",
-		}
-	}
-
-	// Validasi transaksi
-	return types.ResponseCheckTx{
-		Code: 0,
-	}
+func (app *RcpuCoinApp) Info(req abci.RequestInfo) abci.ResponseInfo {
+    return abci.ResponseInfo{
+        Data:             "RCUCoin ABCI",
+        AppVersion:       1,
+        LastBlockHeight:  0,
+        LastBlockAppHash: []byte(""),
+    }
 }
 
-// Fungsi utama untuk menjalankan aplikasi
 func main() {
-	// Cipta aplikasi baru
-	app := NewRcpuCoinApp()
+    app := NewRcpuCoinApp()
 
-	// Mulakan server ABCI untuk aplikasi ini pada port :26658
-	server := types.NewSocketServer(":26658", app)
-	log.Fatal(server.Start())
+    srv, err := server.NewSocketServer("tcp://0.0.0.0:26658", app)
+    if err != nil {
+        log.Fatalf("Error creating ABCI server: %v", err)
+    }
+
+    if err := srv.Start(); err != nil {
+        log.Fatalf("Failed to start ABCI server: %v", err)
+    }
+
+    defer srv.Stop()
+    log.Println("ABCI server running on port 26658")
 }
