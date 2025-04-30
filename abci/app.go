c4adbed59bd7864b08328cce1c4a9471e04bd6fd
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-05-01 03:44:41 +0800

    Update app.go

--- abci/app.go
+++ abci/app.go
@@ -1,50 +1,48 @@
 package abci
 
 import (
-	"encoding/json"
-	"fmt"
-
-	abcitypes "github.com/tendermint/tendermint/abci/types"
+    "encoding/json"
+    "fmt"
+    abcitypes "github.com/tendermint/tendermint/abci/types"
 )
 
 type RCUCoinApp struct {
-	abcitypes.BaseApplication
-	balances map[string]int
+    abcitypes.BaseApplication
+    balances map[string]int64 // simpan dalam Miroc
 }
 
 func NewRCUCoinApp() *RCUCoinApp {
-	return &RCUCoinApp{
-		balances: map[string]int{
-			"address1": 1000,
-			"address2": 500,
-		},
-	}
+    return &RCUCoinApp{
+        balances: map[string]int64{
+            "address1": 5_000_000_000, // 5,000 RCU = 5B Miroc
+            "address2": 2_000_000_000,
+        },
+    }
 }
 
 func (app *RCUCoinApp) Info(req abcitypes.RequestInfo) abcitypes.ResponseInfo {
-	return abcitypes.ResponseInfo{Data: "RCUCoin ABCI App"}
+    return abcitypes.ResponseInfo{Data: "RCUCoin (Miroc-based)"}
 }
 
 func (app *RCUCoinApp) DeliverTx(req abcitypes.RequestDeliverTx) abcitypes.ResponseDeliverTx {
-	var tx struct {
-		From   string `json:"from"`
-		To     string `json:"to"`
-		Amount int    `json:"amount"`
-	}
+    var tx struct {
+        From  string `json:"from"`
+        To    string `json:"to"`
+        Miroc int64  `json:"miroc"` // dalam Miroc
+    }
 
-	err := json.Unmarshal(req.Tx, &tx)
-	if err != nil {
-		return abcitypes.ResponseDeliverTx{Code: 1, Log: "Invalid transaction"}
-	}
+    if err := json.Unmarshal(req.Tx, &tx); err != nil {
+        return abcitypes.ResponseDeliverTx{Code: 1, Log: "Invalid JSON"}
+    }
 
-	if app.balances[tx.From] < tx.Amount {
-		return abcitypes.ResponseDeliverTx{Code: 1, Log: "Insufficient funds"}
-	}
+    if tx.Miroc <= 0 || app.balances[tx.From] < tx.Miroc {
+        return abcitypes.ResponseDeliverTx{Code: 1, Log: "Invalid or insufficient balance"}
+    }
 
-	app.balances[tx.From] -= tx.Amount
-	app.balances[tx.To] += tx.Amount
+    app.balances[tx.From] -= tx.Miroc
+    app.balances[tx.To] += tx.Miroc
 
-	fmt.Printf("Transfer %d from %s to %s\n", tx.Amount, tx.From, tx.To)
+    fmt.Printf("Transferred %d Miroc from %s to %s\n", tx.Miroc, tx.From, tx.To)
 
-	return abcitypes.ResponseDeliverTx{Code: 0, Log: "Success"}
+    return abcitypes.ResponseDeliverTx{Code: 0, Log: "Success"}
 }
