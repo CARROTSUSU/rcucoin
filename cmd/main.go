e9e7bd845b92b175e2d8532ec0630c07439e24a6
FOUNDER OF RCIT <animmmfarhanim@gmail.com>
2025-05-01 03:45:39 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
-	"log"
-
-	"github.com/tendermint/tendermint/abci/server"
-	"github.com/ceocarrotsusu/rcucoin/abci"
+    "log"
+    "github.com/tendermint/tendermint/abci/server"
+    "github.com/tendermint/tendermint/libs/cli"
+    "github.com/username/rcucoin/abci"
 )
 
 func main() {
-	app := abci.NewRCUCoinApp()
-
-	srv, err := server.NewSocketServer("tcp://0.0.0.0:26658", app)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := srv.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	defer srv.Stop()
-	select {}
+    app := abci.NewRCUCoinApp()
+    srv := server.NewSocketServer("tcp://0.0.0.0:26658", app)
+    if err := srv.Start(); err != nil {
+        log.Fatal(err)
+    }
+    defer srv.Stop()
+    cli.TrapSignal(nil, func() {
+        srv.Stop()
+    })
+    select {}
 }
